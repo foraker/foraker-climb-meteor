59b0efdb66941d70478b7c669a1a2c6ac68fdba6
Jon Evans <jle@foraker.com>
2013-12-13 11:22:22 -0700

    Revert "Revert "Add meteor-headers package, ban IPs""
    This reverts commit d3755a53f73e3038188b5eeca27e33d77df78c38.

---
+++ packages/meteor-headers/headers-common.js
@@ -0,0 +1,12 @@
+headers = {
+	list: {},
+	proxyCount: 0,
+	setProxyCount: function(proxyCount) {
+		this.proxyCount = proxyCount;
+	},
+	getProxyCount: function() {
+		return this.proxyCount;
+	}
+}
+
+Package.headers = { headers: headers };
